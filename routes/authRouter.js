277583876ea2feb60107106c6661cb32cfccc20f
Muhammad <timurkaevmuhammad4@gmail.com>
2021-03-31 20:53:04 +0300

    create server

---
+++ routes/authRouter.js
@@ -0,0 +1,17 @@
+const Router = require('express');
+const router = new Router();
+const controller = require('./authController');
+const { check } = require('express-validator');
+const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+
+router.post('/registration', [
+    check("username", "Имя пользователя не может быть пустым").notEmpty(),
+    check("password", "пароль не может быть меньше 4 и больше 10 символов").isLength({ min: 4, max: 10 }),
+], controller.registration);
+
+router.post('/login', controller.login);
+
+router.get('/users', authMiddleware, controller.getUsers);
+
+module.exports = router;
