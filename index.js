fe7dc9561879e76d731d8329329bd92bd4fffa6b
Muhammad <timurkaevmuhammad4@gmail.com>
2021-04-02 19:27:54 +0300

    dsda

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./src/routes/authRouter');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use('/smm', authRouter);
+app.use(express.urlencoded({ extended: true }));
+
+async function start() {
+  try {
+    await mongoose.connect(process.env.MY_BASE, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(process.env.MY_PORT, () => {
+      console.log(`Server has been started on port: ${process.env.MY_PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
