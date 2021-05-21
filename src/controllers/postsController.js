7daf89206eb1d8f95e482315bbe27f13ab91b35d
Muhammad <timurkaevmuhammad4@gmail.com>
2021-05-22 00:39:26 +0300

    fixed problem with draft null 2

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -115,7 +115,7 @@ async function updatePost(req, res) {
     await post.update({
       text: !text ? post.text : text,
       title: !title ? post.title : title,
-      draft: !draft ? post.draft : draft,
+      draft: draft === null ? draft : post.draft,
     });
 
     // Отправляем сообщение об успехе операции
